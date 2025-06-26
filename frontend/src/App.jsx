695b63510f8b4ab2f1dbbe93aed514a9476f95ff
Usman-17 <usmandev580@gmail.com>
2025-06-26 12:39:56 +0500

    Implement login page UI and functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import useGetAuth from "./hooks/useGetAuth";
+import "react-loading-skeleton/dist/skeleton.css";
+import { lazy, Suspense, useEffect } from "react";
+
+// Lazy load pages
+const LoginPage = lazy(() => import("./pages/LoginPage"));
 
 const App = () => {
-  return <div className="bg-red-700">App</div>;
+  const { data: authUser, isLoading } = useGetAuth();
+
+  if (isLoading && !authUser) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-white">
+        <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  };
+
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+
+      <Suspense
+        fallback={
+          <div className="min-h-screen flex items-center justify-center bg-white">
+            <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </Suspense>
+
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          style: {
+            background: "#363636",
+            color: "#fffbfb",
+            fontFamily: "Outfit, sans-serif",
+            fontSize: "14px",
+            padding: "8px 16px",
+          },
+        }}
+      />
+    </BrowserRouter>
+  );
 };
 
 export default App;
